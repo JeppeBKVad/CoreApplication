50b59a0ec38e96c8b3b3fd8bcb600c248aea1665
Jeppe Vad <122011702+JeppeBKVad@users.noreply.github.com>
2023-03-09 07:17:17 +0000

    Init

---
+++ CoreApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CoreApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
