4272173fb1af1a783c3d5d6ecebe682f1cbc3610
Jeppe Vad <122011702+JeppeBKVad@users.noreply.github.com>
2023-03-09 08:03:09 +0000

    added enum permissions

--- CoreApplication/Models/UserPermissionsModel.cs
+++ CoreApplication/Models/UserPermissionsModel.cs
@@ -4,11 +4,11 @@ namespace CoreApplication.Models
     {
         public int Id { get; set; }
         public int User { get; set; }
-        public string Permissions { get; set; } // Edit to enum
+        public Enums.PermissionLevel Permissions { get; set; } // Edit to enum
         public int Last_edited_by { get; set; }
         public DateTime Created_at { get; set; }
         public DateTime Modified_at { get; set; }
-        public User_permissionsModel(int id, int user, string permissions, int last_edited_by, DateTime created_at, DateTime modified_at)
+        public User_permissionsModel(int id, int user, Enums.PermissionLevel permissions, int last_edited_by, DateTime created_at, DateTime modified_at)
         {
             this.Id = id;
             this.User = user;
